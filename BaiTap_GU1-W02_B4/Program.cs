565cb173cb60f6b3b7fd762192e4ff35127740d3
anpham9198 <158065764+anpham9198@users.noreply.github.com>
2024-03-03 15:00:46 +0700

    Initial commit

---
+++ BaiTap_GU1-W02_B4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi9_CauTrucMang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float VND;           
+            Console.Write("Nhap so tien ban muon doi: ");
+            VND = float.Parse(Console.ReadLine());
+
+            float USD = VND/23000;
+            Console.Write("So tien doi ra: " + USD);
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
